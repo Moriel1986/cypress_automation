3c62ac296255e62448c93838aca2999e6cd4e0fd
Demoriel Purnell <43978234+Moriel1986@users.noreply.github.com>
2023-08-05 11:18:38 -0500

    cypress demoblaze test

--- cypress/e2e/demoblazeTest.cy.js
+++ cypress/e2e/demoblazeTest.cy.js
@@ -4,15 +4,14 @@
 
 describe('DemoBlaze Test', () => {
     beforeEach(() => {cy.visit('https://www.demoblaze.com/')
+    cy.clearCookies();
     })
   
     it('Clicks on About Us Top Navigation Button', () => {
         cy.get("a").contains("About us").click();
         //cy.wait(2000);
-        //cy.clearCookies();
         cy.contains('About us').should("be.visible");
 
-      
       })
     })
 
