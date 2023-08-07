82b1e2f4a3ec6b70c3c7378e57644895fbfa7f55
Demoriel Purnell <43978234+Moriel1986@users.noreply.github.com>
2023-08-06 21:40:41 -0500

    cypress order galaxy phone test

--- cypress/e2e/demoblazeTest.cy.js
+++ cypress/e2e/demoblazeTest.cy.js
@@ -1,18 +1,27 @@
 /// <reference types="cypress" />
+import selectors from "./selectors.js";
 
 
 
-describe('DemoBlaze Test', () => {
+describe('DemoBlaze End 2 End Test', () => {
     beforeEach(() => {cy.visit('https://www.demoblaze.com/')
     cy.clearCookies();
     })
   
     it('Clicks on About Us Top Navigation Button', () => {
-        cy.get("a").contains("About us").click();
-        cy.wait(2000);
-        cy.contains('About us').should("be.visible");
-        cy.get("div[id='videoModal'] div[class='modal-footer'] button[type='button']").click();
-        cy.get("body > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(3)").click();
+
+        cy.get(selectors.Log_in).click();
+        cy.wait(1000);
+        cy.get(selectors.Log_in_userName).type(selectors.Username);
+        cy.get(selectors.Log_in_password).type(selectors.Password);
+        cy.get(selectors.Log_in_Btn).click();
+        cy.get(selectors.Phone).click();
+        cy.get(selectors.Samsung_galaxy_s6).click();
+        cy.get(selectors.AddToCart).click();
+        cy.get(selectors.Cart).click();
+
+
+       
 
       })
     })
---
+++ cypress/e2e/selectors.js
@@ -0,0 +1,49 @@
+export default{
+
+///////////////Top Navigation//////////////////  s
+
+Home : "//li[@class='nav-item active']//a[@class='nav-link']",
+Contact : "//a[normalize-space()='Contact']",
+About_us : "//a[normalize-space()='About us']",
+About_us_close : "//div[@id='videoModal']//button[@type='button'][normalize-space()='Close']",
+Cart : "#cartur",
+Log_in : "#login2",
+Log_in_userName : "#loginusername",
+Log_in_password : "#loginpassword",
+Log_in_Btn : "button[onclick='logIn()']",
+Log_in_close : "div[id='logInModal'] div[class='modal-footer'] button:nth-child(1)",
+Sign_up : "#signin2",
+Log_out : "#logout2']",
+AddToCart : ".btn.btn-success.btn-lg",
+PlaceOrder : ".btn.btn-success",
+
+
+/////////////////Categories////////////////////
+
+Phone : "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(2)",
+Laptop: "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(3)",
+Monitors: "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(4)",
+
+
+////////////////////Pagination////////////////
+
+Previous : "//button[@id='prev2']",
+Next : "//button[@id='next2']", 
+
+
+/////////////////////Credentials////////////
+
+Username : "Moriel1986",
+Password : "Kobegoat#0824",
+
+
+/////////////////////Items///////////////////
+
+Samsung_galaxy_s6 : "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)",
+MacBookPro : "//a[normalize-space()='MacBook Pro']",
+AppleMonitor24 : "//a[normalize-space()='Apple monitor 24']",
+
+
+
+
+};
