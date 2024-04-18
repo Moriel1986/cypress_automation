0b04b70a7f3ab8973b3b8063208ade83d620cb6e
Demoriel Purnell <43978234+Moriel1986@users.noreply.github.com>
2024-04-18 06:25:50 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      projectID: '3r5fuc'
       // implement node event listeners here
     },
   },
